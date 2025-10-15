83c0d7d38fb78d67a480142cd5f941cbb624b733
alsgudd0924 <yoominhyung08@gmail.com>
2025-10-15 21:04:06 +0900

    CORS 설정 수정

--- src/main/java/com/it_network/it_network/config/SecurityConfig.java
+++ src/main/java/com/it_network/it_network/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("http://localhost:5173") // 프론트엔드 도메인
+                .allowedOrigins("http://localhost:5174") // 프론트엔드 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
                 .allowedHeaders("*"); // 허용할 헤더
     }
